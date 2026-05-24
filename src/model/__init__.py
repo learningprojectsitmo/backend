1da39a5b56ff568ab84032b998d8cb8eccb9916a
Anton Afanasiev <anton0afanasiev0v@gmail.com>
2026-05-24 08:02:20 +0300

    fix tsets

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,9 +1,8 @@
 from __future__ import annotations
 
 from src.model.audit import AuditLog
-from src.model.auth import PasswordReset, Session
+from src.model.auth import NewUser, PasswordReset, Session
 from src.model.kanban_models import Column, Subtask, Task, TaskAssignee, TaskHistory
-from src.model.auth import NewUser
 from src.model.project import Project, ProjectParticipation, ProjectStatus, ProjectVacancy, Response, Resume, Tag
 from src.model.user import Permission, Role, RolePermission, User, UserPermission
 from src.model.workspace import WorkSpace, WorkSpaceCategories, WorkSpaceParticipation, WorkSpaceStatus
